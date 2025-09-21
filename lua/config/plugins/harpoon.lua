fb8ff30902901824a32e776093f88fd8ad20896d
BasileBux <basile.buxtorf@ik.me>
2025-09-21 19:04:45 +0200

    non-meaningful fixes

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -16,19 +16,6 @@ return {
 
 			vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-			-- Quick navigation to specific marks
-			-- vim.keymap.set("n", "<leader>1", function()
-			-- 	harpoon:list():select(1)
-			-- end, { desc = "Go to harpoon mark 1" })
-			--
-			-- vim.keymap.set("n", "<leader>2", function()
-			-- 	harpoon:list():select(2)
-			-- end, { desc = "Go to harpoon mark 2" })
-			--
-			-- vim.keymap.set("n", "<leader>3", function()
-			-- 	harpoon:list():select(3)
-			-- end, { desc = "Go to harpoon mark 3" })
-
 			for i = 1, 9 do
 				vim.keymap.set("n", "<leader>" .. i, function()
 					harpoon:list():select(i)
