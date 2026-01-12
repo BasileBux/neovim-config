5575a89539587ddf0f9c752c8c04202757ab1052
BasileBux <basile.buxtorf@ik.me>
2026-01-12 18:26:54 +0100

    replaced deprecated telescope functionalities with vim.lsp.buf

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -33,13 +33,15 @@ return {
 					map("rn", vim.lsp.buf.rename, "[R]e[n]ame")
 					map("ga", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 					map("<leader>h", vim.lsp.buf.hover, "[H]over Documentation")
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-					map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+          map("gt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
 					map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-					map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+
+          -- In vim 0.11+ the mapping under this are not relevant
+          -- map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences") -- "grr" after 0.11
+					-- map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation") -- "gri" after 0.11
+					-- map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition") -- "gd" after 0.11
+					-- map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration") -- "gD" after 0.11
+					-- map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols") -- "gO" after 0.11
 
 					-- Highlight references of the word under the cursor (show and clear)
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
